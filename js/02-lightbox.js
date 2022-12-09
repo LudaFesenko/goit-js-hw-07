c62de61b2065b4237ff26694c797b9f2dbff19ed
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-09 21:28:46 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const renderItem = (item) => `
+  <li>
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>`;
 
-console.log(galleryItems);
+const itemsMarkup = galleryItems.map(renderItem).join("");
+
+const gallery = document.querySelector("ul.gallery");
+
+gallery.insertAdjacentHTML("afterbegin", itemsMarkup);
+
+new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionDelay: 250,
+  captionsData: "alt",
+});
